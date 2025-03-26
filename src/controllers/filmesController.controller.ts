c421dd04d8e7034fb170cc3c8fb662719db19448
Johnny <joaogbdesouza@gmail.com>
2025-03-26 19:26:51 -0300

    retornando o resultado de filmes paginado

--- src/controllers/filmesController.controller.ts
+++ src/controllers/filmesController.controller.ts
@@ -3,6 +3,10 @@ import database from "../database/config"
 import { Filme } from "../entidades/filme"
 
 export const getFilmes = async (req: Request, res: Response) => {
+
+  const page: string = req.query.page ?? "1"
+  const perPage: string = req.query.per_page ?? "10"
+
   const filmes: Array<Filme> = await database.
     table('filmes').
     innerJoin("generos", "filmes.genero_id", "=", "generos.id")
@@ -11,6 +15,9 @@ export const getFilmes = async (req: Request, res: Response) => {
       'generos.nome_exibicao as genero',
       'filmes.created_at',
       'filmes.updated_at'
-    ]).returning("*")
+    ]).paginate({
+      perPage: perPage,
+      currentPage: page
+    })
   res.json(filmes)
 }
