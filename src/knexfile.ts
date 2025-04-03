6f4374fb630ee82b6ae1d9a8d597d0677c362c49
Johnny <joaogbdesouza@gmail.com>
2025-04-03 17:39:59 -0300

    adicionando variáveis de ambiente

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,4 +1,5 @@
 import type { Knex } from "knex";
+
 const { attachPaginate } = require('knex-paginate');
 
 attachPaginate();
@@ -7,11 +8,11 @@ const config: { [key: string]: Knex.Config } = {
   development: {
     client: "pg",
     connection: {
-      host: '127.0.0.1',
-      port: 5433,
-      user: 'postgres',
-      password: 'postgres',
-      database: 'api_filmes',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT as string),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
     },
   },
 
