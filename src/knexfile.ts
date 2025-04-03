ed884c79ecfbed8abbd4b402f30ec2c83c54eaa4
Johnny <joaogbdesouza@gmail.com>
2025-04-03 19:14:46 -0300

    substituindo por connection string pra facilitar minha vida

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -8,11 +8,7 @@ const config: { [key: string]: Knex.Config } = {
   development: {
     client: "pg",
     connection: {
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT as string),
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      connectionString: process.env.DATABASE_URL,
     },
   },
 
