03c04f3312df31903012b4d79dc6aa12a4c4784a
Johnny <joaogbdesouza@gmail.com>
2025-04-03 18:02:52 -0300

    mais uma tentativa

--- dist/server.js
+++ dist/server.js
@@ -4,12 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
-var express_2 = require("express");
-var app = express_1.default();
-var route = express_2.Router();
+var dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+var routes_1 = require("./routes");
+var app = (0, express_1.default)();
 app.use(express_1.default.json());
-route.get('/', function (req, res) {
-    res.json({ message: 'hello world with Typescript' });
-});
-app.use(route);
+var v1Routes = (0, routes_1.v1)();
+app.use('/api/v1', v1Routes);
 app.listen(3333, function () { return 'server running on port 3333'; });
