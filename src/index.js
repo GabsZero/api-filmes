3717d5e729a28c159d80526df58297339b1e30ad
Johnny <joaogbdesouza@gmail.com>
2025-04-03 18:49:50 -0300

    nem sei mais xD

---
+++ src/index.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+var routes_1 = require("./routes");
+var app = (0, express_1.default)();
+app.use(express_1.default.json());
+var v1Routes = (0, routes_1.v1)();
+app.use('/api/v1', v1Routes);
+app.listen(process.env.PORT, function () { return "server running on port ".concat(process.env.PORT); });
+module.exports = app;
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 
-import { v1 } from "./src/routes";
+import { v1 } from "./routes";
 
 const app = express();
 app.use(express.json())
